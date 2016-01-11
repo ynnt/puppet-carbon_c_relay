ab29fa884c9ef9ab52f4385597114ac8f1d95c37
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-01-11 01:57:26 +0100

    Added params user and group

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,14 @@
 #
 class carbon_c_relay::params {
   $config_file                 = '/etc/carbon-c-relay.conf'
+  $group                       = 'carbon-c-relay'
   $interface                   = 'all'
   $listen                      = 2003
   $output_file                 = undef
   $package_ensure              = present
   $package_manage              = true
   $package_name                = 'carbon-c-relay'
+  $pid_dir                     = '/var/run/carbon-c-relay'
   $server_batch_size           = 2500
   $server_queue_size           = 25000
   $service_enable              = true
@@ -14,5 +16,6 @@ class carbon_c_relay::params {
   $service_manage              = true
   $service_name                = $module_name
   $statistics_sending_interval = 60
+  $user                        = 'carbon-c-relay'
   $worker_threads              = 1
 }
