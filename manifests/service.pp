7923ccd90d0368803bd7e8495b5fc12c0160e1f3
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-01-11 01:57:09 +0100

    Use of systemd erb template

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,9 +6,11 @@ class carbon_c_relay::service inherits carbon_c_relay {
   }
 
   if $carbon_c_relay::service_manage == true {
-    exec { "systemd_reload":
-      command     => '/bin/systemctl daemon-reload',
-      refreshonly => true,
+    file { '/etc/systemd/system/carbon-c-relay.service':
+      ensure  => $carbon_c_relay::service_ensure,
+      content => template($carbon_c_relay::init_template),
+      before  => Service[$carbon_c_relay::service_name],
+      notify  => Service[$carbon_c_relay::service_name]
     }
 
     service { $carbon_c_relay::service_name:
@@ -17,8 +19,7 @@ class carbon_c_relay::service inherits carbon_c_relay {
       name       => $carbon_c_relay::service_name,
       hasstatus  => true,
       hasrestart => true,
-      provider   => 'systemd',
-      require    => Exec['systemd_reload']
+      provider   => 'systemd'
     }
   }
 }
