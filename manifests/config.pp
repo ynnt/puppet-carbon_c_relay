73cb9ee51c5537a2ce4b8b2cd37edeb36bb6ee9a
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-01-13 01:54:27 +0100

    Cleared double declaration

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,4 @@
 #
 class carbon_c_relay::config inherits carbon_c_relay {
 
-  file { $carbon_c_relay::sysconfig_file:
-    ensure  => file,
-    content => template($carbon_c_relay::sysconfig_template),
-    before  => Service[$carbon_c_relay::service_name],
-    notify  => Service[$carbon_c_relay::service_name]
-  }
 }
