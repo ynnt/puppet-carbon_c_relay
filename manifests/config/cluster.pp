66cf4b12b447686bcb51b691d324ecff9bb71d87
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-01-17 02:59:01 +0100

    Added validations to cluster. Enhanced template

--- manifests/config/cluster.pp
+++ manifests/config/cluster.pp
@@ -1,10 +1,26 @@
 #
 define carbon_c_relay::config::cluster (
   $cluster_name       = $title,
+  $file               = undef,
   $forward_proto      = 'forward',
-  $hosts              = [],
+  $hosts              = {},
   $replication_factor = 1
 ){
+
+  validate_string($cluster_name)
+  validate_integer($replication_factor)
+
+  if ! ($file == undef) {
+    validate_string( $file )
+  }
+
+  if ! ($forward_proto in ['forward', 'file']) {
+    fail("forward_proto '${forward_proto}' is not in ['forward', 'file']")
+  }
+
+  unless is_hash( $hosts ) {
+    fail("'hosts' param is not a hash")
+  }
 
   concat::fragment { "cluster-${title}":
     target  => $carbon_c_relay::config_file,
