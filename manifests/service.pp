2fea59c80c2a6d7119c72d36ef90aaab5e72af73
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-02-29 14:29:50 +0100

    Init file should be executable

--- manifests/service.pp
+++ manifests/service.pp
@@ -17,6 +17,7 @@ class carbon_c_relay::service inherits carbon_c_relay {
 
     file { $carbon_c_relay::init_file:
       ensure  => $carbon_c_relay::init_file_ensure,
+      mode    => '0755',
       content => template($carbon_c_relay::init_template),
       before  => Service[$carbon_c_relay::service_name],
       notify  => Service[$carbon_c_relay::service_name]
