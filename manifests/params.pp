2a7d40771dee5b44ec09787d2e9eec0a489fff3f
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-08-09 13:26:34 +0200

    Corrected version to 0.3 in metadata
    Removed unnecessary comma
    
    Updated dependencies in metadata.json
    
    Bumped version to 0.2.0
    
    Set sane defaults for integer params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 #
 class carbon_c_relay::params {
   $allowed_chars               = undef
-  $carbon_cache_statistics     = undef
+  $carbon_cache_statistics     = false
   $config_clusters             = {}
   $config_file                 = '/etc/carbon-c-relay.conf'
   $config_matches              = {}
@@ -11,17 +11,17 @@ class carbon_c_relay::params {
   $init_file_ensure            = 'file'
   $init_template               = "carbon_c_relay${init_file}.erb"
   $interface                   = 'all'
-  $io_timeout                  = undef
+  $io_timeout                  = 600
   $limit_fsize                 = undef
   $limit_cpu                   = undef
   $limit_as                    = undef
   $limit_no_file               = 64000
   $limit_nproc                 = 64000
   $listen                      = 2003
-  $listen_backlog              = undef
+  $listen_backlog              = 3
   $log_dir                     = '/var/log/carbon-c-relay'
   $log_file                    = 'carbon-c-relay.log'
-  $max_stalls                  = undef
+  $max_stalls                  = 4
   $output_file                 = undef
   $package_ensure              = latest
   $package_manage              = true
